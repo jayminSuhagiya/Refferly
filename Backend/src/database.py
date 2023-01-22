672cfec78f755899a3299656bf66ab3fde962db7
ozxx <ozxx@localhost.localdomain>
2023-01-21 21:49:05 -0500

    docker-compose setup

--- Backend/src/database.py
+++ Backend/src/database.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import config as config
 
-SQLALCHEMY_DATABASE_URL = "postgresql://newuser:password@localhost:5432/refferaly"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{config.DB_USER}:{config.DB_PASSWORD}@{config.DB_HOST}:{config.DB_PORT}/{config.DB_NAME}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
